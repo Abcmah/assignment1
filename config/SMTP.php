6876e5269f076811607301b5a356f04897ec390c
Bismarck Momanyi <63294360+Abcmah@users.noreply.github.com>
2023-07-29 16:39:23 +0300

    Update SMTP.php

--- config/SMTP.php
+++ config/SMTP.php
@@ -4,7 +4,7 @@ $my_smtp='smtp.gmail.com'; //your smtp eg smtp.gmail.com
 $port =465;//your smtp port
 $smtp_auth=true;
 $smtp_secure="ssl";
-$my_password ="mnvfpjxahotfyvys"; // your app password
+$my_password ="your password here"; // your app password
 $my_username ="bismarquemomanyi@gmail.com"; // you username or email e.g. john@john.com
 $send_to = "bismarquemomanyi@gmail.com";//mail to
 ?>
